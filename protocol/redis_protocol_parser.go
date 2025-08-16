5127c3174939cf00d70cbc57029b36dd2455e958
AntonyChR <antonychr99@gmail.com>
2025-08-16 11:50:41 -0500

    add: implement RedisProtocolParser with encoding and decoding methods

---
+++ protocol/redis_protocol_parser.go
@@ -0,0 +1,53 @@
+package protocol
+
+
+import (
+	"strconv"
+)
+
+type RedisProtocolParser struct{}
+
+func (r *RedisProtocolParser) Encode(data []string) (string, error){
+	if len(data) > 0 || data[0] == REPLCONF || data[0] == PING{
+		return r.EncodeAsArray(data), nil
+	} else {
+		return "+" + data[0] + ENDL , nil
+	}
+}
+
+// bulkString returns a Redis bulk string representation of the given data.
+// If addEnd is true, a line break is added at the end of the string.
+func (p *RedisProtocolParser) EncodeBulkString(data string, addEnd bool) string {
+	content := "$" + strconv.Itoa(len(data)) + "\r\n" + data
+	if addEnd {
+		content += "\r\n"
+	}
+	return content
+}
+
+func (r *RedisProtocolParser) EncodeError(msg string) []byte {
+	return []byte("-ERR " + msg + ENDL)
+}
+
+func (r *RedisProtocolParser) EncodeAsArray(data []string) string {
+	content := "*" + strconv.Itoa(len(data)) + "\r\n" + "$" + strconv.Itoa(len(data[0])) + "\r\n" + data[0] + "\r\n"
+	for _, arg := range data[1:]{
+		content += "$" + strconv.Itoa(len(arg)) + "\r\n" + arg + "\r\n"
+	}
+
+	return content
+}
+
+func (r *RedisProtocolParser) NullBulkString() []byte {
+ return []byte{36, 45, 49, 13, 10} // $-1\r\n
+}
+
+func (r *RedisProtocolParser) Ok() []byte {
+ return []byte{43, 79, 75, 13, 10} // +OK\r\n
+}
+
+func (r *RedisProtocolParser) Decode(data []byte) ([]string, error){
+	return parseData(data), nil
+}
+
+
