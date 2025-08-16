9e4df52f3326913d306fcbd3d0aaf6c28273b587
AntonyChR <antonychr99@gmail.com>
2025-08-15 22:12:00 -0500

    refactor: update Execute method signatures in command handlers to return error

--- command/concrete_handlers.go
+++ command/concrete_handlers.go
@@ -2,34 +2,37 @@ package command
 
 import (
 	"context"
+	"errors"
 	"redisgo/protocol"
 	storage "redisgo/storage"
 )
 
 type PingHandler struct{}
 
-func (p *PingHandler) Execute(ctx *context.Context){
+func (p *PingHandler) Execute(ctx *context.Context) ([]byte, error) {
+	return []byte("+PONG\r\n"), nil
 }
 
-type GetHandler struct{
+type GetHandler struct {
 	Storage *storage.Storage
-	parser protocol.Parser
+	parser  protocol.Parser
 }
 
-func (g *GetHandler) Execute( args []string, ctx *context.Context) ([]byte, error){
-	value, _:= g.Storage.Get(args[0])
-	encondedResp:= g.parser.EncodeBulkString(value, true) 
+func (g *GetHandler) Execute(args []string, ctx *context.Context) ([]byte, error) {
+	value, _ := g.Storage.Get(args[0])
+	encondedResp := g.parser.EncodeBulkString(value, true)
 	return []byte(encondedResp), nil
 }
 
-
-type SetHandler struct{
-	Storage *storage.Storage
+type SetHandler struct {
+	Storage     *storage.Storage
 	ReplicaChan chan []byte
 }
 
-func (s *SetHandler) Execute( args []string, ctx *context.Context) ([]byte, error){
-	s.Storage.Set("","")
+func (s *SetHandler) Execute(args []string, ctx *context.Context) ([]byte, error) {
+	if len(args[0]) == 0 {
+		return nil, errors.New("invalid key value")
+	}
+	s.Storage.Set(args[0], args[1])
 	return nil, nil
 }
-
