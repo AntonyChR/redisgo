4fe70f2c1f799a691fa86803272666f012183e3e
AntonyChR <antonychr99@gmail.com>
2025-08-16 11:50:30 -0500

    refactor: improve Redis server structure and connection handling in main and network packages

--- main.go
+++ main.go
@@ -1,14 +1,96 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"io"
+	"log"
+	"net"
+
+	network "redisgo/network"
+
+	command "redisgo/command"
+	protocol "redisgo/protocol"
+	storage "redisgo/storage"
 )
 
 var SERVER_PORT = flag.String("port", "6379", "Port to listen on")
 var REPLICA_OF = flag.String("replicaof", "", "Replicate to another server")
 
+type Redis struct {
+	server *network.TcpServer
+	parser protocol.Parser
+	handlers map[string] command.CommandHandler
+	ctx context.Context
+}
+
+func (r *Redis) Start(){
+	r.server.Start(r.handleConnection)
+
+}
+
+func (r *Redis) handleConnection(conn net.Conn) {
+	buff := make([]byte, 1024)
+	defer conn.Close()
+	for {
+		n, err := conn.Read(buff)
+
+		if err == io.EOF {
+			log.Println("EOF, connection closed", err)
+			return
+		}
+
+		if err != nil {
+			log.Println("error reading data, ", err)
+			return
+		}
+
+		if n == 0 {
+			log.Println("data length: 0")
+			return
+		}
+
+		data := buff[:n]
+
+		decodedData, _ := r.parser.Decode(data)
+		commands,_ := command.ExtractCommandsFromParsedData(decodedData)
+
+		for _, c := range commands {
+			if handler, ok := r.handlers[c.Name]; ok {
+				resp, _ := handler.Execute(c.Args, &r.ctx)
+				conn.Write(resp)
+			}
+		}
+
+
+	}
+}
+
+
 func main() {
 	flag.Parse()
-	println("RedisGo Server started")
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	storage := storage.NewStorage()
+
+	p := &protocol.RedisProtocolParser{}
+	replicaChan := make(chan []byte)
+
+	handlers := make(map[string]command.CommandHandler)
+	handlers["get"] = &command.GetHandler{Storage: storage}
+	handlers["set"] = &command.SetHandler{Storage: storage, ReplicaChan: replicaChan}
+
+	server, _ := network.CreateNewServer("3000", "master", "")
+
+	redis := Redis{
+		server: server,
+		handlers: handlers,
+		parser: p,
+		ctx: ctx,
+	}
+
+	redis.Start()
 
 }
--- network/server.go
+++ network/server.go
@@ -12,16 +12,16 @@ import (
 
 // server roles
 const (
-	MASTER = "master" 
-	SLAVE = "slave"
+	MASTER = "master"
+	SLAVE  = "slave"
 )
 
 type Data struct {
-	Id string
+	Id   string
 	Data []byte
 }
 
-func CreateNewServer(port string, role string, masterAddr string) (*TcpServer, error){
+func CreateNewServer(port string, role string, masterAddr string) (*TcpServer, error) {
 	if role != SLAVE && role != MASTER {
 		errorMsg := fmt.Sprintf("invalid role option, expected 0(master) or 1(slave) got: %d", role)
 		return nil, errors.New(errorMsg)
@@ -32,106 +32,49 @@ func CreateNewServer(port string, role string, masterAddr string) (*TcpServer, e
 	}
 
 	return &TcpServer{
-		Read: make(chan Data),
-		Write: make(chan Data),
+		Read:                 make(chan Data),
+		Write:                make(chan Data),
 		RegisterNewSlaveChan: make(chan string),
-		conn: make(map[string]net.Conn),
-		context: context.Background(),
+		conn:                 make(map[string]net.Conn),
 	}, nil
 
 }
 
-type TcpServer struct{
-	Read chan Data 
-	Write chan Data 
-	port string
+type TcpServer struct {
+	Read                 chan Data
+	Write                chan Data
+	CloseConn            chan string
+	port                 string
 	RegisterNewSlaveChan chan string
-	conn  map[string] net.Conn
-	context context.Context
+	conn                 map[string]net.Conn
+	context              context.Context
 }
 
-func (s *TcpServer) Listen() error{
-	listener, err := net.Listen("tcp", "0.0.0.0:" + s.port)
+func (s *TcpServer) Start(handleConn func(net.Conn)) error {
+	listener, err := net.Listen("tcp", "0.0.0.0:"+s.port)
 	if err != nil {
 		return err
 	}
+	defer listener.Close()
 
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			return err
-		}
-		go s.handleConnection(conn)
-	}
-}
+	errChan := make(chan error, 1)
 
-func (s* TcpServer) handleConnection(conn net.Conn){
-	buff := make([]byte, 1024)
-
-	n, err := conn.Read(buff)
-
-	if err == io.EOF {
-		log.Println("EOF, connection closed", err)
-		return
-	}
-
-	if err != nil {
-		log.Println("error reading data, ", err)
-		return
-	}
-
-	if n == 0 {
-		log.Println("data length: 0")
-		return
-	}
-
-	conId := utils.GenerateUUID()
-
-	s.conn[conId] = conn
-	
-	s.Read <- Data{conId,buff[:n]}
-
-}
-
-func (s *TcpServer) send(){
-	for{
-		select{
-		case data := <- s.Read:
-
-			if conn, ok := s.conn[data.Id]; ok {
-				n, err := conn.Write(data.Data)
+	go func(){
+		for {
+			conn, err := listener.Accept()
 				if err != nil {
-					log.Println("error writting in connection, ", err)
-					continue
+					errChan <- err
+					return 
 				}
-
-				if n == 0 {
-					log.Println("data writted in connection was 0")
-					continue
-				}
-				conn.Close()
-				delete(s.conn, data.Id)
-			}
-
-		case <- s.context.Done():
-			return
+			go handleConn(conn)
 		}
+	}()
+
+	select {
+	case <- s.context.Done():
+		return nil
+	case err := <- errChan:
+		return err
 	}
 }
 
-func (s *TcpServer) recv(){
-	for{
-		select{
-		case data := <- s.Write:
-			log.Println(string(data.Data))
-			// send to parser/command extractor -> execute
-		case <- s.context.Done():
-			return
-		}
-	}
-}
-
-func (s *TcpServer) Start(){
-	go s.send()
-	go s.recv()
-}
