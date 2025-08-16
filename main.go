15fdd2afa1441ecbc2aed2fcdaa9bd7e5a4ce5bd
AntonyChR <antonychr99@gmail.com>
2025-08-16 12:51:16 -0500

    refactor: improve error handling in Redis connection and command execution

--- main.go
+++ main.go
@@ -18,18 +18,19 @@ var SERVER_PORT = flag.String("port", "6379", "Port to listen on")
 var REPLICA_OF = flag.String("replicaof", "", "Replicate to another server")
 
 type Redis struct {
-	server *network.TcpServer
-	parser protocol.Parser
-	handlers map[string] command.CommandHandler
-	ctx context.Context
+	server   *network.TcpServer
+	parser   protocol.Parser
+	handlers map[string]command.CommandHandler
+	ctx      context.Context
 }
 
-func (r *Redis) Start(){
+func (r *Redis) Start() {
 	r.server.Start(r.handleConnection)
 
 }
 
 func (r *Redis) handleConnection(conn net.Conn) {
+	log.Printf("New connection from %s\n", conn.RemoteAddr().String())
 	buff := make([]byte, 1024)
 	defer conn.Close()
 	for {
@@ -52,20 +53,34 @@ func (r *Redis) handleConnection(conn net.Conn) {
 
 		data := buff[:n]
 
-		decodedData, _ := r.parser.Decode(data)
-		commands,_ := command.ExtractCommandsFromParsedData(decodedData)
+		decodedData, err := r.parser.Decode(data)
+		if err != nil {
+			log.Println("error decoding data, ", err)
+			return
+		}
+		commands, err := command.ExtractCommandsFromParsedData(decodedData)
+		if err != nil {
+			log.Println("error extracting commands, ", err)
+			return
+		}
 
 		for _, c := range commands {
+			log.Printf("Received command: %s with args: %v\n", c.Name, c.Args)
 			if handler, ok := r.handlers[c.Name]; ok {
-				resp, _ := handler.Execute(c.Args, &r.ctx)
+				resp, err := handler.Execute(c.Args, &r.ctx)
+				if err != nil {
+					log.Println("error executing command, ", err)
+					return
+				}
 				conn.Write(resp)
+			} else {
+				log.Printf("Unknown command: %s\n", c.Name)
+				conn.Write([]byte("-ERR unknown command '" + c.Name + "'\r\n"))
 			}
 		}
 
-
 	}
 }
-
 
 func main() {
 	flag.Parse()
@@ -79,16 +94,17 @@ func main() {
 	replicaChan := make(chan []byte)
 
 	handlers := make(map[string]command.CommandHandler)
+	handlers["ping"] = &command.PingHandler{}
 	handlers["get"] = &command.GetHandler{Storage: storage}
 	handlers["set"] = &command.SetHandler{Storage: storage, ReplicaChan: replicaChan}
 
 	server, _ := network.CreateNewServer("3000", "master", "")
 
 	redis := Redis{
-		server: server,
+		server:   server,
 		handlers: handlers,
-		parser: p,
-		ctx: ctx,
+		parser:   p,
+		ctx:      ctx,
 	}
 
 	redis.Start()
