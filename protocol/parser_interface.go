f04d5d103eabb4fe3caa3d3f4eddf79d127489b9
AntonyChR <antonychr99@gmail.com>
2025-08-16 11:51:09 -0500

    refactor: update Parser interface to use string slices for Encode method and add additional encoding methods

--- protocol/parser_interface.go
+++ protocol/parser_interface.go
@@ -1,7 +1,11 @@
 package protocol
 
 type Parser interface{
-	Encode(data string) ([]byte, error)
+	Encode(data []string) (string, error)
 	EncodeBulkString(data string, addEnd bool) string
+	EncodeError(msg string) []byte
+	EncodeAsArray(data []string) string
+	NullBulkString() []byte 
+	Ok() []byte
 	Decode(data []byte) ([]string, error)
 }
