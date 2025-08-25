025198e4fed67b561ab08dd6f34d78b066b55112
AntonyChR <antonychr99@gmail.com>
2025-08-25 18:25:58 -0500

    add: concatenate array and map to array functions

--- protocol/parser_interface.go
+++ protocol/parser_interface.go
@@ -5,6 +5,8 @@ type Parser interface{
 	EncodeBulkString(data string, addEnd bool) string
 	EncodeAsSimpleString(data string, addEnd bool) string
 	EncodeError(msg string) []byte
+	ConcatenateArray(data []string) string 
+	MapToArray(data map[string]string) string
 	EncodeAsArray(data []string) string
 	NullBulkString() []byte 
 	Ok() []byte
